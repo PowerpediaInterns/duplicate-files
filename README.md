00910d35e86af4cce6d77ff1cb5c07db0b8e397a
srubalcava002 <srubalcava002@gmail.com>
2020-11-10 18:18:28 -0600

    add duplicatefiles.py and documentation

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # duplicate-files
+Script that adds Template:Duplicate files to any file listed on Special:ListDuplicatedFiles. Also contains a reusable function for adding templates to a list of files.
---
+++ duplicatefiles.py
@@ -0,0 +1,69 @@
+import pywikibot
+import requests
+import json
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def getURL():
+	"""
+	Gets the API URL of the wiki.
+
+	Returns:
+		URL (str): API url of the wiki
+	"""
+
+	site = pywikibot.getSite()
+	URL = site.protocol() + "://" + site.hostname() + site.scriptpath() + "/api.php"
+	return URL
+
+def getAllDuplicateFiles(URL):
+	"""
+	Gets a list of all the page titles of files listed on Special:ListDuplicatedFiles.
+
+	Parameters:
+		URL (str): the API URL of the wiki
+	
+	Returns:
+		output (list): a list with all files' page title on Special:ListDuplicatedFiles
+	"""
+
+	PARAMS = {
+	"action": "query",
+	"generator": "allimages",
+	"prop": "duplicatefiles",
+	"format": "json"
+	}
+
+	session = requests.Session()
+	request = session.get(url=URL, params=PARAMS, verify=False)
+	outputJson = request.json()
+
+	output = []
+	for page in outputJson["query"]["pages"]:
+		output.append(outputJson["query"]["pages"][page]["title"])
+		
+	return output
+
+def addTemplate(template, pages):
+	"""
+	Adds a template to pages in a list.
+
+	Parameters:
+		template (str): the template to add
+		pages (list): a list of page titles
+	"""
+
+	for title in pages:
+		page = pywikibot.Page(pywikibot.Site(), title)
+		text = page.text
+        
+		if (text.find(template) != -1):
+			print("Template found! Skipping %s..." % (page))
+		else:
+			print("Template not found on %s! Appending %s..." % (page, template))
+			page.text += template
+			page.save(u"Add " + template)
+
+if __name__ == "__main__":
+	addTemplate("{{Duplicate files}}", getAllDuplicateFiles(getURL()))
